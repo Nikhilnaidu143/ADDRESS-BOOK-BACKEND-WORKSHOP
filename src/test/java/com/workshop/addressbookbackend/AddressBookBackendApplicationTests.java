bdb0502324dbabc79f100747f4453b814358e01a
saitarun135 <nikhilnaidu473@gmail.com>
2022-01-28 05:12:12 +0530

    START:- Created spring project for address book backend app.

---
+++ src/test/java/com/workshop/addressbookbackend/AddressBookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.addressbookbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
