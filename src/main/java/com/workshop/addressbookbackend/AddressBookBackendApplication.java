65f3d23d2b6a7f46739fb9c1d21cae0923127ae3
saitarun135 <nikhilnaidu473@gmail.com>
2022-01-28 08:28:48 +0530

    UC-3.2:- Used lombok library for logging.

--- src/main/java/com/workshop/addressbookbackend/AddressBookBackendApplication.java
+++ src/main/java/com/workshop/addressbookbackend/AddressBookBackendApplication.java
@@ -3,12 +3,16 @@ package com.workshop.addressbookbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class AddressBookBackendApplication {
 
 	/*** Main method. ***/
 	public static void main(String[] args) {
 		SpringApplication.run(AddressBookBackendApplication.class, args);
+		log.info("Address Book App Started...!");
 	}
 
 }
