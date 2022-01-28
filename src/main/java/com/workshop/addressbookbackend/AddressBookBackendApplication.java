19919728bdeda0abe9e9cff33757cd778ff1d55b
saitarun135 <nikhilnaidu473@gmail.com>
2022-01-28 09:54:17 +0530

    UC-3.3:- Created application.properties for dev and prod environments.

--- src/main/java/com/workshop/addressbookbackend/AddressBookBackendApplication.java
+++ src/main/java/com/workshop/addressbookbackend/AddressBookBackendApplication.java
@@ -2,6 +2,7 @@ package com.workshop.addressbookbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,8 +12,8 @@ public class AddressBookBackendApplication {
 
 	/*** Main method. ***/
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookBackendApplication.class, args);
-		log.info("Address Book App Started...!");
+		ApplicationContext context = SpringApplication.run(AddressBookBackendApplication.class, args);
+		log.info("Address Book App started in {} Environmet.", context.getEnvironment().getProperty("environment"));
 	}
 
 }
